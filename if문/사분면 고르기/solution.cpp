b7ff0e972c1632c356825a3204282d45aa43967f
uyeah0 <79914479+uyeah0@users.noreply.github.com>
2021-07-24 20:22:27 +0900

    210724

---
+++ if문/사분면 고르기/solution.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int x,y;
+    cin >> x;
+    cin >> y;
+    if(x > 0){
+        if(y > 0) cout << 1;
+        else cout << 4;
+    }else if(x < 0){
+        if(y > 0) cout << 2;
+        else cout << 3;
+    }
+
+    return 0;
+}
