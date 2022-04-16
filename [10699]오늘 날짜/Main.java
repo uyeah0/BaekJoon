5bf7e6c96178ca32c77b8d9e3c570654800d387a
Yeyeong Yu <79914479+uyeah0@users.noreply.github.com>
2022-04-16 15:53:35 +0900

    Add files via upload

---
+++ [10699]오늘 날짜/Main.java
@@ -0,0 +1,10 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Main{
+    public static void main(String []args){
+        LocalDate now = LocalDate.now();
+        
+        System.out.println(now);
+    }
+}
