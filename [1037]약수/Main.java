ca753b91a605def9664f7c3311a5fe058f619413
Yeyeong Yu <79914479+uyeah0@users.noreply.github.com>
2022-05-03 23:14:20 +0900

    Add files via upload

---
+++ [1037]약수/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.util.*;
+
+public class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i =0; i < arr.length; i++){
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        
+        System.out.println(arr[0]*arr[arr.length-1]);
+    }
+}
