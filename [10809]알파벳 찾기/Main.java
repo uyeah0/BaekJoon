df1c04c3e29236c61baebf448569eae367a51b00
Yeyeong Yu <79914479+uyeah0@users.noreply.github.com>
2022-05-14 22:50:33 +0900

    Update and rename 문자열/알파벳 찾기/Main.java to [10809]알파벳 찾기/Main.java

---
+++ [10809]알파벳 찾기/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String args[]){
+	 Scanner sc = new Scanner(System.in);
+     
+	 String s = sc.next();
+	 int alpha[] = new int[26];
+	 int print[] = new int[26];
+
+	 for(int i = 0; i < 26; i++){
+	 	alpha[i] = i+97;
+		print[i] = -1;
+	 }
+
+	 for(int i =0; i < s.length(); i++){
+		for(int j = 0; j < 26; j++){
+			if((char)alpha[j] == s.charAt(i)){
+				if(print[j] == -1) {
+					print[j] = i;	
+				}
+				break;
+		        }
+		 }
+	 }
+
+	 for(int i = 0; i < 26; i++) {
+	 	System.out.print(print[i] + " ");
+	 }
+    }
+}
